2a2e03f6150ff2618f8725d7cf0cd55028e84edc
JuanCarrillo2435 <juancarrillo.dc@gmail.com>
2023-10-17 17:28:06 -0300

    Pasando bien

--- Transversal/src/Data/InscripcionData.java
+++ Transversal/src/Data/InscripcionData.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Data;
 
-/**
- *
- * @author juanb
- */
+import Conexion.Conexion;
+import Entidades.Alumno;
+import Entidades.Inscripcion;
+import java.sql.*;
+import java.util.List;
+
 public class InscripcionData {
+
+    private Connection con;
+    private MateriaData matData;
+    private AlumnoData alumData;
+
+    public InscripcionData() {
+        con= Conexion.getConexion();
+    }
+
+    public void guardarInscripcion(Inscripcion insc) {
+
+    }
+
+    List<Inscripcion> obtenerInscripciones() {
+
+        return null;
+    }
+
+    List<Inscripcion> obtenerInscripcionesPorAlumno(int id) {
+
+        return null;
+    }
+
+    List<Inscripcion> obtenerMateriasCursadas(int id) {
+
+        return null;
+    }
+
+    List<Inscripcion> obtenerMateriasNOCursadas(int id) {
+
+        return null;
+    }
     
+    public void borrarInscripcionMateriaAlumno(int idAlumno, int idMateria){
+        
+    }
+    
+    public void actualizarNota(int idAlumno, int idMateria, double nota){
+        
+    }
+    
+    List<Alumno> obtenerAlumnosXMateria (int idMateria){
+        
+        return null;
+    }
+
 }
--- Transversal/src/Data/MateriaData.java
+++ Transversal/src/Data/MateriaData.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Data;
 
-/**
- *
- * @author juanb
- */
+import Conexion.Conexion;
+import Entidades.Materia;
+import java.sql.*;
+import java.util.List;
+
 public class MateriaData {
+    private Connection con;
+
+    public MateriaData() {
+        con = Conexion.getConexion();
+    }
+    
+    public void guardarMateria(Materia materia){
+        
+    }
+    
+    Materia buscarMateria(int id){
+        
+        return null;
+    }
+    
+    public void modificarMateria(Materia materia){
+        
+    }
+    
+    public void eliminarMateria(int id){
+        
+    }
+    
+    List<Materia> listarMateria(){
+        
+        
+        return null;
+    }
+    
     
 }

d3a09ca554e62ab26782292b90090916746d4273
JuanCarrillo2435 <juancarrillo.dc@gmail.com>
2023-10-19 19:23:51 -0300

    metodo obtener inscripciones

--- Transversal/src/Data/InscripcionData.java
+++ Transversal/src/Data/InscripcionData.java
@@ -3,6 +3,7 @@ package Data;
 import Conexion.Conexion;
 import Entidades.Alumno;
 import Entidades.Inscripcion;
+import Entidades.Materia;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +15,10 @@ public class InscripcionData {
     private MateriaData matData;
     private AlumnoData alumData;
     private Inscripcion insc;
-    
+
     public InscripcionData() {
         con = Conexion.getConexion();
-        
+
     }
 
     public void guardarInscripcion(Inscripcion insc) {
@@ -29,12 +30,12 @@ public class InscripcionData {
             ps.setInt(2, insc.getAlumno().getIdAlumno());
             ps.setInt(3, insc.getMateria().getIdMateria());
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
-            
+
             if (rs.next()) {
                 insc.setIdInscripto(rs.getInt(1));
-                JOptionPane.showMessageDialog(null, "Inscripcion exito");    
+                JOptionPane.showMessageDialog(null, "Inscripcion exito");
             }
             ps.close();
         } catch (SQLException ex) {
@@ -43,9 +44,35 @@ public class InscripcionData {
 
     }
 
-    List<Inscripcion> obtenerInscripciones() {
-        List<Inscripcion> insc = new ArrayList();
-        return null;
+    public List<Inscripcion> obtenerInscripciones() {
+        List<Inscripcion> inscripciones = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM inscripcion";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Inscripcion inscripcion = new Inscripcion();
+                inscripcion.setIdInscripto(rs.getInt("idInscripto"));
+                inscripcion.setNota(rs.getInt("nota"));
+
+                // Obtener información relacionada de Alumno y Materia y asignarla a Inscripcion
+                Alumno alumno = new Alumno();
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                inscripcion.setAlumno(alumno); // Supongamos que Inscripcion tiene un método setAlumno
+
+                Materia materia = new Materia();
+                materia.setIdMateria(rs.getInt("idMateria"));
+                inscripcion.setMateria(materia); // Supongamos que Inscripcion tiene un método setMateria
+
+                inscripciones.add(inscripcion);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Inscripcion: " + ex.getMessage());
+        }
+
+        return inscripciones;
     }
 
     List<Inscripcion> obtenerInscripcionesPorAlumno(int id) {
